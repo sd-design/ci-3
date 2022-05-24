b783820c28005b6216589b7d5a849d5a7691fa5c
Alex Solovey <naghtigall@gmail.com>
2022-05-24 18:20:35 +0300

    Начал прорабатывать Гугл-Таблицы для Патмоса

---
+++ codein/application/controllers/Sheets.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH. 'vendor/autoload.php';
+
+class Sheets extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *	 GENERAL controller class
+	 */
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Sheets_model', 'sheet');
+    }
+//	public function index()
+//	{
+//        $sheetId = "1YfotWSx2w6HTxdmerR5JAFNO2Joeax4eQDfY3bWD378";
+//        $get_range = "LeadsFromTilda";
+//        $this->sheet->readSheet($sheetId, $get_range);
+//
+//		echo APPPATH;
+//	}
+    public function index()
+    {
+        $sheetId = "1iFGI3rVyVHxHHVdAC3-dNwtavHzw4QNOuBoLyvtOwNQ";
+        $get_range = "patmos_members!A2:I";
+        $this->sheet->readSheet($sheetId, $get_range);
+    }
+
+    public function read()
+    {
+        $sheetId = "1iFGI3rVyVHxHHVdAC3-dNwtavHzw4QNOuBoLyvtOwNQ";
+        $get_range = "patmos_members!A2:I";
+        $this->sheet->readSheet($sheetId, $get_range);
+    }
+
+    public function registration()
+    {
+        $data = $this->sheet->fail_data();
+        var_dump($data);
+    }
+}
---
+++ codein/application/models/Sheets_model.php
@@ -0,0 +1,100 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sheets_model extends CI_Model
+{
+    private $credentials = APPPATH. 'vendor/ap-landing-1529413281194-e2577855840f.json';
+    private $account = 'naghtigall@gmail.com';
+
+    public function readSheet($spreadsheetId, $get_range){
+
+        $client = new \Google_Client();
+        $client->setApplicationName('PHPsheets');
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        $client->setAuthConfig($this->credentials);
+        $service = new \Google_Service_Sheets($client);
+        $response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
+        $values = $response->getValues();
+
+        $count = count($values)+1;
+        $this->output
+            ->set_status_header(200)
+            ->set_content_type('application/json', 'utf-8')
+            ->set_output(json_encode($values, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+            ->_display();
+        exit;
+
+        //echo $count."\n";
+    }
+
+    public function insertData($sheetID, $spreadsheetId, $values = false){
+        if($values){
+            $insert_values = $values;
+        }
+        else{
+            $insert_values = $this->getPOST();
+        }
+
+
+        $client = new \Google_Client();
+        $client->setApplicationName('PHPsheets');
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        $client->setAuthConfig($this->credentials);
+        $service = new Google_Service_Sheets($client);
+
+        $append_range = $sheetID.'!A:E';
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $insert_values
+        ]);
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+        $result = $service->spreadsheets_values->append($spreadsheetId, $append_range,
+            $body, $params);
+        $this->response($result, $spreadsheetId);
+    }
+
+    private function getPOST(){
+        if(empty($_POST)){
+            return null;
+        }
+        else{
+            //Basic inputs define
+            $name = self::setValue('name_');
+            $surname = self::setValue('lastname_');
+            $church= self::setValue('church_');
+            $city = self::setValue('city_');
+            $phone = self::setValue('phone_');
+            $email = self::setValue('email_');
+            $dateFrom = self::setValue('dateFrom_');
+            $dateTo = self::setValue('dateFrom_');
+            //var_dump($nominations);exit;
+            //Utm-params
+            $insert_values =  [[
+                $surname,
+                $name,
+                $church,
+                $city,
+                $phone,
+                $dateFrom,
+                $dateTo,
+                date("Y-m-d H:i:s"),
+                $_SERVER['HTTP_HOST']
+            ]];
+            return $insert_values;
+        }
+
+    }
+
+    private function setValue($value){
+        $getValue = empty($_POST[$value]) ? "Undefined" : $_POST[$value];
+        return $getValue;
+    }
+    public function fail_data()
+    {
+        $insert_values = $this->getPOST();
+        return $insert_values;
+
+    }
+}
